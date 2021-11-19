b7b0de17b9028e508d3f92cd7c9d70b1ad7961ec
Curry <zhonggl@meix.com>
2021-11-19 15:23:15 +0800

    demo--111

--- curry.js
+++ curry.js
@@ -1,23 +1,3 @@
-console.log('1')
-console.log('2')
-console.log('3')
-console.log('4')
-console.log('5')
-console.log('6')
-console.log('7')
-console.log('8')
-console.log('dev4')
-
-console.log('9')
-console.log('10')
-console.log('dev5')
-
-console.log('11')
-console.log('12')
-console.log('dev1')
-console.log('dev2')
-console.log('dev3')
-
 
 console.log('demo-----111')
 console.log('demo-----222')
